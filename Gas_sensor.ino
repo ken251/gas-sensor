f97094a14db109ec8ebc7ddb2517a0e1cdd2574f
Kennedy Wambua <kennywambu254@gmail.com>
2021-02-19 13:27:18 +0300

    v1.0.1

--- Gas_sensor.ino
+++ Gas_sensor.ino
@@ -1,5 +1,6 @@
 // Digital pin 8 will be called 'pin8'
 int pin8 = 8;
+int pin9 = 9;
 // Analog pin 0 will be called 'sensor'
 int sensor = A0;
 // Set the initial sensorValue to 0
@@ -9,6 +10,7 @@ int sensorValue = 0;
 void setup() {
   // Initialize the digital pin 8 as an output
   pinMode(pin8, OUTPUT);
+  pinMode(pin9, OUTPUT);
   // Initialize serial communication at 9600 bits per second
   Serial.begin(9600);
 }
@@ -19,14 +21,20 @@ void loop() {
   sensorValue = analogRead(sensor);
   // Print out the value you read
   Serial.println(sensorValue, DEC);
-  // If sensorValue is greater than 500
-  if (sensorValue > 300) {
+  if (sensorValue > 100) {
     // Activate digital output pin 8 - the LED will light up
     //digitalWrite(pin8, HIGH);
     digitalWrite(pin8, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(300);                       // wait for a second
   digitalWrite(pin8, LOW);    // turn the LED off by making the voltage LOW
   delay(300);  
+  if(sensorValue > 118)
+ {
+ // delay(10000);
+  digitalWrite(pin9,HIGH);//beep goes in after 10sec
+  delay(300);
+  digitalWrite(pin9,LOW);//beep goes on and off
+ }
   }
   else {
     // Deactivate digital output pin 8 - the LED will not light up
