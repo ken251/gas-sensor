cf608ad5895e023a657e293a810840dc8e928f5c
Kennedy Wambua <kennywambu254@gmail.com>
2021-02-20 09:57:28 +0300

    v1.0.2

--- Gas_sensor.ino
+++ Gas_sensor.ino
@@ -1,13 +1,19 @@
 // Digital pin 8 will be called 'pin8'
+#include <WaziDev.h>
+#define TRIG 11 //Module pins
+#define ECHO 12 
 int pin8 = 8;
 int pin9 = 9;
+int pin10 = 10;
 // Analog pin 0 will be called 'sensor'
 int sensor = A0;
 // Set the initial sensorValue to 0
 int sensorValue = 0;
 
 // The setup routine runs once when you press reset
+WaziDev wazidev("gaspro", 5);
 void setup() {
+  wazidev.setup();
   // Initialize the digital pin 8 as an output
   pinMode(pin8, OUTPUT);
   pinMode(pin9, OUTPUT);
@@ -16,11 +22,36 @@ void setup() {
 }
 
 // The loop routine runs over and over again forever
-void loop() {
-  // Read the input on analog pin 0 (named 'sensor')
+void loop() 
+{ 
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2); 
+    
+  digitalWrite(TRIG, HIGH); 
+  delayMicroseconds(20); 
+    
+  digitalWrite(TRIG, LOW);
+  int distance = pulseIn(ECHO, HIGH,26000); 
+    
+  distance= distance/58; //Convert the pulse duration to distance
+                           //You can add other math functions to calibrate it well
+                           
+    Serial.print("Distance ");
+    Serial.print(distance);
+    Serial.println("cm");
+   
+    delay(1000);
+ 
   sensorValue = analogRead(sensor);
   // Print out the value you read
   Serial.println(sensorValue, DEC);
+  delay(300);
+  if (sensorValue <110 ){
+    digitalWrite(pin10,HIGH);
+     
+  }else{
+    digitalWrite(pin10,LOW);
+   
   if (sensorValue > 100) {
     // Activate digital output pin 8 - the LED will light up
     //digitalWrite(pin8, HIGH);
@@ -28,17 +59,23 @@ void loop() {
   delay(300);                       // wait for a second
   digitalWrite(pin8, LOW);    // turn the LED off by making the voltage LOW
   delay(300);  
-  if(sensorValue > 118)
- {
- // delay(10000);
-  digitalWrite(pin9,HIGH);//beep goes in after 10sec
+  } else if(distance < 50 && sensorValue >70){
+   digitalWrite(pin10,LOW);
+    digitalWrite(pin8, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(pin8, LOW);    // turn the LED off by making the voltage LOW
+  delay(300);  
+       digitalWrite(pin9,HIGH);//beep goes on after 0.3sec
   delay(300);
   digitalWrite(pin9,LOW);//beep goes on and off
- }
-  }
+  delay(300);
+    }
   else {
     // Deactivate digital output pin 8 - the LED will not light up
     digitalWrite(pin8, LOW);
   }
 
-}
+  }
+   WaziDev::SensorVal fields[2] = {{"DIS", distance}, {"GAS", sensorValue}};
+   wazidev.sendSensorValues(fields, 2);
+  }
